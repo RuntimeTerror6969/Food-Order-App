e97ff19accabee4ba3f52ab6078ece407f86f9f3
adarsh <aadarsh.22.2003@gmail.com>
2023-06-03 16:06:49 +0530

    First Commit

---
+++ src/components/Meals/AvailableMeals.js
@@ -0,0 +1,68 @@
+import { useEffect, useState } from "react";
+
+import Card from "../UI/Card";
+import MealItem from "./MealItem/MealItem";
+import classes from "./AvailableMeals.module.css";
+
+const AvailableMeals = () => {
+  const [meals, setMeals] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState();
+  useEffect(() => {
+    setIsLoading(false);
+    const fetchMeals = async () => {
+      const response = await fetch(
+        "https://react-postrequest-38f67-default-rtdb.firebaseio.com/meals.json"
+      );
+      const responseData = await response.json();
+
+      if (!response.ok) {
+        throw new Error("Failed to fetch data from requested server ");
+      }
+
+      const loadedMeals = [];
+
+      for (const key in responseData) {
+        loadedMeals.push({
+          id: key,
+          name: responseData[key].name,
+          description: responseData[key].description,
+          price: responseData[key].price,
+        });
+      }
+
+      setMeals(loadedMeals);
+    };
+
+    fetchMeals().catch((error) => {
+      setIsLoading(false);
+      setError(error.message);
+    });
+  }, []);
+
+  if (isLoading) {
+    return <p className={classes.loading}>Loading....</p>;
+  }
+  if (error) {
+    return <p className={classes.error}>{error}</p>;
+  }
+  const mealsList = meals.map((meal) => (
+    <MealItem
+      key={meal.id}
+      id={meal.id}
+      name={meal.name}
+      description={meal.description}
+      price={meal.price}
+    />
+  ));
+
+  return (
+    <section className={classes.meals}>
+      <Card>
+        <ul>{mealsList}</ul>
+      </Card>
+    </section>
+  );
+};
+
+export default AvailableMeals;
